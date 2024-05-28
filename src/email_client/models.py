97fdaf904a96ff326193fa2cf2dcb148cd2172b0
TaleeBarbieri <barbieri.talee@gmail.com>
2024-05-28 20:13:52 +0100

    initial commit

---
+++ src/email_client/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.utils.translation import gettext_lazy as _
+
+
+# Create your models here.
+
+
+class CustomUser(AbstractUser):  # This is a custom user
+    first_name = models.CharField(_("first name"), max_length=150, blank=True)
+    last_name = models.CharField(_("last name"), max_length=150, blank=True)
+    email = models.EmailField(_("email address"), null=True, blank=True)
+    profile_picture = models.ImageField(null=True, blank=True)
+    last_login = models.DateTimeField(null=True, blank=True)
+
+    groups = models.ManyToManyField(Group, blank=True)
+    user_permissions = models.ManyToManyField(Permission, blank=True)
+
+    def __str__(self):
+        return self.username
